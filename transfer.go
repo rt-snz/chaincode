4e667816f772f5cdbfdf30bb78230283a759456b
senzaki <ryota.senzaki@mizuho-ir.co.jp>
2017-04-30 14:43:35 +0900

    fix syntax error

--- transfer.go
+++ transfer.go
@@ -186,8 +186,8 @@ func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, function strin
 
 func (t *SimpleChaincode) cert(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
-	var user,key_password string
-	var password_state_bytes []bytes;
+	var user, password, key_password string
+	var password_state_bytes []byte;
 	var err error;
 
 	user = args[0];
